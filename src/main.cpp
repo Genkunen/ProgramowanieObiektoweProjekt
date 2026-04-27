8e8f2d34fdc6ceba6f34ab890f915bfe366dd674
2B <yoruhanigoubgataa65042@tuta.com>
2026-04-28 00:01:14 +0200

    [Initial] Setup basic SDL window and event loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
-#include <print>
+#include <SDL3/SDL.h>
 
 auto main() -> int {
-    std::println("hai :3");
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("ProgramowanieObiektoweProjekt", 1920, 1080, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 }
