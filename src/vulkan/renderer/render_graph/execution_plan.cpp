5816a7c2cc6c5ad9e71dc1c949178b80a62bd67b
2B <yoruhanigoubgataa65042@tuta.com>
2026-05-06 12:29:31 +0200

    [Vulkan] Use references to resources in the pipeline barrier node

--- src/vulkan/renderer/render_graph/execution_plan.cpp
+++ src/vulkan/renderer/render_graph/execution_plan.cpp
@@ -7,13 +7,13 @@ void PipelineBarrierNode::execute(vk::raii::CommandBuffer& cmd, const Simulation
     std::vector<vk::ImageMemoryBarrier2> image_barriers;
 
     for (auto& [buffer_id, barrier] : m_buffer_barriers) {
-        auto res = resources.get_buffer_by_identifier(buffer_id);
+        auto& res = resources.get_buffer_by_identifier(buffer_id);
         barrier.buffer = res.vk_buffer();
         buffer_barriers.push_back(barrier);
     }
 
     for (auto& [image_id, barrier] : m_image_barriers) {
-        auto res = resources.get_image_by_identifier(image_id);
+        auto& res = resources.get_image_by_identifier(image_id);
         barrier.image = res.vk_image();
         image_barriers.push_back(barrier);
     }
