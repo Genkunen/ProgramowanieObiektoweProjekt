5b4316dc3e1ec6c94d9a7c7bab7bd7e13db7a8d0
2B <yoruhanigoubgataa65042@tuta.com>
2026-05-08 18:15:23 +0200

    [Vulkan] Fix coordinate system

--- src/vulkan/renderer/vk_renderer.cpp
+++ src/vulkan/renderer/vk_renderer.cpp
@@ -42,7 +42,7 @@ inline glm::mat4 build_projview(glm::vec3 pos, float aspect_ratio) {
     // must flip Y for Vulkan
     proj[1][1] *= -1.0f;
 
-    glm::mat4 view = glm::lookAt(pos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 view = glm::lookAtLH(pos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     return proj * view;
 }
 
@@ -204,14 +204,14 @@ auto VulkanRenderer::render_frame(MeshPool& mesh_pool, const std::span<const Mes
     // ---- Fill simulation data buffer ------------------------------------------------------------------------------------------------------------------------
 
     float aspect_ratio = static_cast<float>(m_main_render_target.extent().width) / static_cast<float>(m_main_render_target.extent().height);
-    glm::mat4 projview = build_projview({0.0f, 0.0f, 20.0f}, aspect_ratio);
+    glm::mat4 projview = build_projview({0.0f, 0.0f, -20.0f}, aspect_ratio);
     float time_since_start = std::chrono::duration<float>(std::chrono::high_resolution_clock::now() - m_start_timepoint).count();
 
     shaders::SimulationData simulation_data = shaders::SimulationData{projview, delta_time, time_since_start};
 
     memcpy(frame.simulation_data_buffer.memory_host_ptr(), &simulation_data, sizeof(shaders::SimulationData));
 
-    // ---- Preinitialize and/or refit simulation if needeed ---------------------------------------------------------------------------------------------------
+    // ---- Preinitialize and/or refit simulation if needed ----------------------------------------------------------------------------------------------------
 
     if (m_gpu_driven_sim_needs_refit || m_gpu_driven_sim_needs_preinit) device.waitIdle(); // to avoid hazard with CS reading the buffer, this is rare anyway so it's fine
 
