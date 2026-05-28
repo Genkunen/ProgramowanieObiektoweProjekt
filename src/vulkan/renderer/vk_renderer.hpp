58ed2a83274e80cb1a4798c47862bca0fa007d04
2B <yoruhanigoubgataa65042@tuta.com>
2026-05-28 09:32:58 +0200

    [Vulkan] Increase default object count

--- src/vulkan/renderer/vk_renderer.hpp
+++ src/vulkan/renderer/vk_renderer.hpp
@@ -15,7 +15,7 @@ struct ImDrawData;
 namespace pop::vulkan::renderer {
 
 constexpr size_t MAX_FRAMES_IN_FLIGHT = 2;
-constexpr uint64_t DEFAULT_GPU_DRIVEN_SIM_OBJECT_COUNT = 40000;
+constexpr uint64_t DEFAULT_GPU_DRIVEN_SIM_OBJECT_COUNT = 100000;
 
 enum class RenderResult {
     Ok,
