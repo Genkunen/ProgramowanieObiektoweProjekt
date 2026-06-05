59dbffc5b314e10d898f2eb6bc644e819ec23da9
2B <yoruhanigoubgataa65042@tuta.com>
2026-06-05 10:44:53 +0200

    Add support for specialization constants, use new radix sort model with equal wave lane count and group size

--- src/vulkan/renderer/radix_sort.hpp
+++ src/vulkan/renderer/radix_sort.hpp
@@ -3,11 +3,20 @@
 #include "simulation_render_graph_passes.hpp"
 
 #include <cstdint>
+#include <print>
 
 namespace pop::vulkan::renderer {
 
+constexpr std::uint32_t get_radix_sort_group_size() {
+    return VulkanContext::get().physical_device_vulkan13_properties().maxSubgroupSize;
+}
+
+constexpr std::uint32_t get_radix_sort_keys_count_per_group() {
+    return get_radix_sort_group_size() * shader_consts::CS_SIMULATION_ACCELERATION_GRID_RADIX_SORT_HISTOGRAM_BUILD_KEYS_PER_THREAD;
+}
+
 constexpr std::uint64_t compute_memory_needed_for_radix_sort_group_histograms(std::uint32_t object_count) {
-    std::uint32_t group_count = div_ceil(object_count, shader_consts::CS_SIMULATION_ACCELERATION_GRID_RADIX_SORT_HISTOGRAM_BUILD_KEYS_PER_GROUP);
+    std::uint32_t group_count = div_ceil(object_count, get_radix_sort_keys_count_per_group());
 
     std::uint64_t total_dwords = static_cast<std::uint64_t>(group_count)
         * static_cast<std::uint64_t>(shader_consts::CS_SIMULATION_ACCELERATION_GRID_RADIX_SORT_HISTOGRAM_RADIX_BUCKETS);
--- src/vulkan/vk_compute_pipeline.hpp
+++ src/vulkan/vk_compute_pipeline.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "vk_compute_pipeline.hpp"
 #include "vk_graphics_pipeline.hpp"
+#include "vk_specialization_constants_map.hpp"
 
 namespace pop::vulkan {
 
@@ -22,9 +23,9 @@ public:
     constexpr VulkanComputePipelineBuilder() = default;
 
     VulkanComputePipelineBuilder(const VulkanComputePipelineBuilder&) = delete;
-    VulkanComputePipelineBuilder(VulkanComputePipelineBuilder&&) = default;
+    VulkanComputePipelineBuilder(VulkanComputePipelineBuilder&&) = delete;
     VulkanComputePipelineBuilder& operator=(const VulkanComputePipelineBuilder&) = delete;
-    VulkanComputePipelineBuilder& operator=(VulkanComputePipelineBuilder&&) = default;
+    VulkanComputePipelineBuilder& operator=(VulkanComputePipelineBuilder&&) = delete;
 
     [[nodiscard]] constexpr auto set_shader(const SpirvCode& shader_code) -> VulkanComputePipelineBuilder& {
         auto stage_tuple = vk::StructureChain{
@@ -37,20 +38,51 @@ public:
         m_shader_stage_create_info = stage_tuple;
         return *this;
     }
+
+    template <typename T>
+    [[nodiscard]] constexpr auto set_shader(const SpirvCode& shader_code, const VulkanSpecializationConstantsMap<T>& specialization_constants) -> VulkanComputePipelineBuilder& {
+        m_specialization_info = vk::SpecializationInfo()
+            .setPData(&specialization_constants.data())
+            .setDataSize(sizeof(T))
+            .setMapEntries(specialization_constants.map_entries());
+
+        auto stage_tuple = vk::StructureChain{
+            vk::PipelineShaderStageCreateInfo()
+                .setStage(vk::ShaderStageFlagBits::eCompute)
+                .setPName("main")
+                .setPSpecializationInfo(&m_specialization_info),
+            shader_code.vulkan_shader_module_create_info(),
+            vk::PipelineShaderStageRequiredSubgroupSizeCreateInfo{}
+        };
+        m_shader_stage_create_info = stage_tuple;
+        return *this;
+    }
+
     [[nodiscard]] constexpr auto set_pipeline_layout(const VulkanPipelineLayout& layout) noexcept -> VulkanComputePipelineBuilder& {
         m_pipeline_layout = layout.vk_pipeline_layout();
         return *this;
     }
     [[nodiscard]] constexpr auto set_preferred_wave_lane_count(uint32_t lane_count) noexcept -> VulkanComputePipelineBuilder& {
         assert(lane_count >= 2 && (lane_count & (lane_count - 1)) == 0 && "lane_count must be a power of 2");
-        m_preferred_wave_lane_count = lane_count;
+        auto& device_vk13_props = VulkanContext::get().physical_device_vulkan13_properties();
+
+        uint32_t clamped_wave_lane_count = std::clamp(lane_count, device_vk13_props.minSubgroupSize, device_vk13_props.maxSubgroupSize);
+        m_set_wave_lane_count = clamped_wave_lane_count;
+        return *this;
+    }
+    [[nodiscard]] constexpr auto set_required_wave_lane_count(uint32_t lane_count) -> VulkanComputePipelineBuilder& {
+        assert(lane_count >= 2 && (lane_count & (lane_count - 1)) == 0 && "lane_count must be a power of 2");
+        auto& device_vk13_props = VulkanContext::get().physical_device_vulkan13_properties();
+
+        if (lane_count > device_vk13_props.maxSubgroupSize || lane_count < device_vk13_props.minSubgroupSize)
+            throw std::runtime_error("required lane_count is outside the range of the device's minSubgroupSize and maxSubgroupSize");
+
+        m_set_wave_lane_count = lane_count;
         return *this;
     }
     [[nodiscard]] constexpr auto build() -> VulkanComputePipeline {
-        auto& device_vk13_props = VulkanContext::get().physical_device_vulkan13_properties();
-        if (m_preferred_wave_lane_count) {
-            uint32_t wave_lane_count = std::clamp(*m_preferred_wave_lane_count, device_vk13_props.minSubgroupSize, device_vk13_props.maxSubgroupSize);
-            m_shader_stage_create_info.get<vk::PipelineShaderStageRequiredSubgroupSizeCreateInfo>().requiredSubgroupSize = wave_lane_count;
+        if (m_set_wave_lane_count) {
+            m_shader_stage_create_info.get<vk::PipelineShaderStageRequiredSubgroupSizeCreateInfo>().requiredSubgroupSize = *m_set_wave_lane_count;
         } else {
             m_shader_stage_create_info.unlink<vk::PipelineShaderStageRequiredSubgroupSizeCreateInfo>();
         }
@@ -68,7 +100,8 @@ private:
     vk::PipelineLayout m_pipeline_layout{ nullptr };
     vk::StructureChain<vk::PipelineShaderStageCreateInfo, vk::ShaderModuleCreateInfo, vk::PipelineShaderStageRequiredSubgroupSizeCreateInfo> m_shader_stage_create_info{};
 
-    std::optional<uint32_t> m_preferred_wave_lane_count{};
+    std::optional<uint32_t> m_set_wave_lane_count{};
+    vk::SpecializationInfo m_specialization_info{};
 };
 
 constexpr auto VulkanComputePipeline::builder() -> VulkanComputePipelineBuilder { return VulkanComputePipelineBuilder(); }
